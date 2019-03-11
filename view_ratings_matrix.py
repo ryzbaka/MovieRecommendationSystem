d0cb15bb29e4eb712788b703c778b3e85a9e0ed1
Hamza <hamzaali151214@hotmail.com>
2019-03-11 22:02:42 +0300

    "Added python programs to factorize the previously generated review matrix and fill the null values by predicting user reviews for each movie, hence filling all null values"

--- view_ratings_matrix.py
+++ view_ratings_matrix.py
@@ -5,6 +5,7 @@ import webbrowser
 
 df=pd.read_csv("movie_ratings_data_set.csv")
 revised_df=pd.pivot_table(df,index="user_id",columns="movie_id",aggfunc=np.max)
+revised_df.to_csv("review_matrix2.csv")
 html=revised_df.to_html(na_rep="")
 
 with open("review_matrix.html","w") as f:
